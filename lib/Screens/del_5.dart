273a1510933432d3c9bc836a275f4f0fb766a6b8
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-09 18:43:12 +0530

    create del5 srceen and userDetails screen

--- lib/Screens/del_5.dart
+++ lib/Screens/del_5.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snapy/bottomnavbar_option5/userDetails_Screen.dart';
 
 
 class Del5 extends StatefulWidget {
@@ -24,29 +25,30 @@ class _Del5State extends State<Del5> {
                   isScrollControlled: true,
                     context: context,
                     builder: (context){
-                    return userDetails();
+
+                    return UserDetails();
                     });
               },
               child: IconButton(
                 onPressed: (){},
                   icon: Container(
                     height: 30,width: 30,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Colors.white,
                       shape: BoxShape.circle,
                     ),
-                    child: Icon(Icons.supervised_user_circle,
+                    child: const  Icon(Icons.supervised_user_circle,
                     color: Colors.black, size: 30,),
                   )),
             ),
             IconButton(
-              icon: Icon(Icons.search, color: Colors.white, size: 30,),
+              icon: const Icon(Icons.search, color: Colors.white, size: 30,),
                 onPressed: (){
                 Navigator.push(
                   context, MaterialPageRoute(builder: (_) => null)
                 );
                 }, ),
-            Expanded(child: Center(
+            const Expanded(child: Center(
               child: Text(
                 "Discover",
                 style: TextStyle(
@@ -73,9 +75,94 @@ class _Del5State extends State<Del5> {
                       return;
                       });
                 },
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: 45,
+                    right: 8,
+                  ),
+                  child: Icon(Icons.add_circle_outline_sharp, color: Colors.white, size: 30,),
+                ),
               )
             ],
           )
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.black,),
+          ListView(padding: const EdgeInsets.only(top: 8, left: 10),children: [
+            Container(
+              child: const Text("Discover subscription", style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),),
+            ),
+            Container(
+              height: 170,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(
+                  right: 8,
+                  bottom: 8,
+                  top: 4,
+                ),
+                children: [],
+              ),
+            ),
+            SizedBox(height: 8,),
+            Container(
+              child: const Text(
+                "Continue Watching",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            Container(
+              height: 170,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(
+                  right: 8,
+                  bottom: 8,
+                  top: 4,
+                ),
+                children: [],
+              ),
+            ),
+            const SizedBox(height: 8,),
+            Container(
+              child: const Text(
+                "For You",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700
+                ),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.only(
+              right: 10,
+              top: 2,
+            ),
+            child: GridView.count(crossAxisCount: 8,
+              childAspectRatio: 1 / 1.6,
+              mainAxisSpacing: 13,
+              crossAxisSpacing: 2,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: [],
+            ),
+            ),
+            const SizedBox(height: 10,),
+          ],
+           )
         ],
       ),
     );
---
+++ lib/bottomnavbar_option5/userDetails_Screen.dart
@@ -0,0 +1,115 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_swipe_detector/flutter_swipe_detector.dart';
+
+class UserDetails extends StatefulWidget {
+  const UserDetails({Key? key}) : super(key: key);
+
+  @override
+  State<UserDetails> createState() => _UserDetailsState();
+}
+
+class _UserDetailsState extends State<UserDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 5,
+            automaticallyImplyLeading: false,
+            title: Padding(padding: const EdgeInsets.all(18),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.settings, color: Colors.black,
+                  ),
+                  onTap: (){
+                    Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => null));
+                  },
+                )
+              ],
+            ),),
+          ),
+        ),
+      ),
+      body: SwipeDetector(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Container(child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: const EdgeInsets.only(top:18 ),
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                ),),
+                const Padding(padding: EdgeInsets.only(top: 15),
+                child: Text(
+                  "Hazer",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),),
+                const Padding(padding: EdgeInsets.only(top: 8),
+                child: Text("callme_haz * 34,456 * U",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+                ),),
+
+                const Padding(padding: EdgeInsets.only(top: 30),
+                child: Icon(
+                  Icons.gavel_sharp,
+                  color: Colors.grey,
+                  size: 30,
+                ),),
+                const Padding(padding: EdgeInsets.only(top: 15),
+                child: Text("Joined Snapchat on 06 June 2023",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700
+                ),
+                ),),
+                const SizedBox(height: 90,),
+              ],
+            ),
+          ),),
+        ),
+        onSwipeLeft: (context){
+          Navigator.pop(context);
+        },
+        onSwipeRight: (context){
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
