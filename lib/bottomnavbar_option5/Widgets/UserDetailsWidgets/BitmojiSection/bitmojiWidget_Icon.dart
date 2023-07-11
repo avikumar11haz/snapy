a7eaa90b091a5b5ddf5a87b7cf19987821a400b2
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-11 19:25:26 +0530

    create bitmoji widget and bitmoji widget icon

---
+++ lib/bottomnavbar_option5/Widgets/UserDetailsWidgets/BitmojiSection/bitmojiWidget_Icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget bitmojiIcon(icon){
+  return Padding(padding: const EdgeInsets.only(left: 8),
+  child: Icon(
+    icon, color: Colors.grey[350],
+    size: 30,
+  ),);
+}
---
+++ lib/bottomnavbar_option5/Widgets/UserDetailsWidgets/BitmojiSection/bitmoji_Widget1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:snapy/bottomnavbar_option5/Widgets/UserDetailsWidgets/BitmojiSection/bitmojiWidget_Icon.dart';
+
+Widget bitmojiWidget1(){
+  return Padding(padding: const EdgeInsets.only(
+    left: 15, right: 15, top: 8
+  ),
+  child: Container(
+    height: 60,
+    decoration: const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(12),
+        topRight: Radius.circular(12),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black38,
+          offset: Offset(0, 2),
+          blurRadius: 1,
+          spreadRadius: 1,
+        )
+      ]
+    ),
+    child: Row(
+      children: [
+        Row(
+          children: [
+            //TODO: Bitmoji Icon
+            bitmojiIcon(Icons.shopping_cart),
+            //TODO: BitmojiText2
+          ],
+        ),
+        Spacer(),
+        // TODO: bitmoji Row
+      ],
+    ),
+
+  ),);
+}
