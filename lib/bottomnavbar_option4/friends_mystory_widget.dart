f4267cf4fdd9c397dd5d27ac46f4012095adc416
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-13 22:51:57 +0530

    friends section complete in friends my story widget file

---
+++ lib/bottomnavbar_option4/friends_mystory_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+Widget friendsMyStory() {
+  return Padding(padding: const EdgeInsets.only(
+    bottom: 4.0
+  ),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Container(
+        child: Text(
+          "Friends",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 20
+          ),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[350],
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 3.0, bottom: 3, left: 3, right: 6),
+          child: Row(
+            children: [
+              Icon(
+                Icons.add,
+                color: Colors.grey,
+              ),
+              Text(
+
+              )
+            ],
+          ),
+        ),
+      )
+    ],
+  ),);
+}
