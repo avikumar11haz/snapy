0edd890127b38268de8f6759a7df5ac346921a80
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-20 00:31:53 +0530

    add bitmoji item map settings widget in map settings screen

--- lib/bottomnavbar_option1/widgets/bitmojiItemMapSettings_widget.dart
+++ lib/bottomnavbar_option1/widgets/bitmojiItemMapSettings_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget bitmojiItemMapSettings(){
   return Container(
-    padding: EdgeInsets.only(
+    padding: const EdgeInsets.only(
         top: 12, bottom: 12, left: 15, right: 15),
     child: Row(
       children: [
